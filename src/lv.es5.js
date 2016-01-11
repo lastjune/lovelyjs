6140ab54b273ee521bcedd6c7844ba2cd336dc27
Timiz Qi <uncledu@aliyun.com>
2016-01-11 14:47:42 +0800

    添加times方法

--- src/lv.es5.js
+++ src/lv.es5.js
@@ -10,9 +10,22 @@
 export let lv = {
     // 判断n是否为素数
     isPrime(n) {
-        if (typeof Number(n) !== 'number') {
-            throw 'The stdin is NaN.';
+            if (typeof Number(n) !== 'number') {
+                throw 'The stdin is NaN.';
+            }
+            return n < 2 ? false : !/^(11+?)\1+$/.test(Array(n + 1).join('1'));
+        },
+        //执行n次fn
+        times(fn, n) {
+            if(typeof n !=="number"){
+                throw TypeError("请输入数字作为想要执行的次数");
+            }
+            return ()=> {
+                if (--times > 0) {
+                    if (typeof fn == 'function')
+                        return fn.apply(this, arguments);
+                }
+                if (times < 1) return undefined;
+            };
         }
-        return n < 2 ? false : !/^(11+?)\1+$/.test(Array(n + 1).join('1'));
-    }
 };
