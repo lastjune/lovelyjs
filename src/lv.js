b0f0c9d08659cee67024a097aa1870d545e90536
Timiz Qi <uncledu@aliyun.com>
2016-03-10 11:53:29 +0800

    update

--- src/lv.js
+++ src/lv.js
@@ -6,20 +6,20 @@
  * @author Timiz Qi
  * @date 2016年1月4日 星期一
  */
-export var lv =()=> {
+export var lv ={
     const argToArray=(arg)=>{
         return (arg.length===1?[arg[0]]:[].apply(null,arguments))||[];
-    };
+    },
     //mixin
     const mixin=(target,source)=>{
         //todo
-    };
+    },
     const isPrime=(n)=> {
         if (typeof Number(n) !== 'number'||n===void 0) {
             throw 'The stdin is NaN.';
         }
         return n < 2 ? false : !/^(11+?)\1+$/.test(Array(n + 1).join('1'));
-    };
+    },
     //sort an array by the reg expression or default by number
     const sortByReg=(arr, reg)=> {
         var defaultReg = /\d/;
@@ -29,12 +29,12 @@ export var lv =()=> {
         return arr.sort(function(a, b) {
             return a.match(defaultReg) - b.match(defaultReg);
         });
-    };
+    },
     //reflection of the function parameter list
     const getParams=(func)=> {
         var fnStr = func.toString().replace(/((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg, '');
         return fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(/([^\s,]+)/g) || [];
-    };
+    },
     //delegateFunc
     const delegateFunc=(func,params)=>{
         if (!params) return func;
@@ -45,5 +45,5 @@ export var lv =()=> {
         };
         f.getParams = function () { return paramNames; };
         return f;
-    };
+    }
 };
