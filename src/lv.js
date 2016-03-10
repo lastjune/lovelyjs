c425b665ed91f7989edc318d716f4cd23d66a714
Timiz Qi <uncledu@aliyun.com>
2016-03-10 13:28:00 +0800

    update

--- src/lv.js
+++ src/lv.js
@@ -7,21 +7,21 @@
  * @date 2016年1月4日 星期一
  */
 export var lv ={
-    const argToArray=(arg)=>{
+    argToArray(arg){
         return (arg.length===1?[arg[0]]:[].apply(null,arguments))||[];
     },
     //mixin
-    const mixin=(target,source)=>{
+    mixin(target,source){
         //todo
     },
-    const isPrime=(n)=> {
+    isPrime(n){
         if (typeof Number(n) !== 'number'||n===void 0) {
             throw 'The stdin is NaN.';
         }
         return n < 2 ? false : !/^(11+?)\1+$/.test(Array(n + 1).join('1'));
     },
     //sort an array by the reg expression or default by number
-    const sortByReg=(arr, reg)=> {
+    sortByReg(arr, reg){
         var defaultReg = /\d/;
         if (reg) {
             defaultReg = reg;
@@ -31,12 +31,12 @@ export var lv ={
         });
     },
     //reflection of the function parameter list
-    const getParams=(func)=> {
+    getParams(func){
         var fnStr = func.toString().replace(/((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg, '');
         return fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(/([^\s,]+)/g) || [];
     },
     //delegateFunc
-    const delegateFunc=(func,params)=>{
+    delegateFunc(func,params){
         if (!params) return func;
         var paramNames = this.getParams(func);
         var f = function () {
@@ -45,5 +45,8 @@ export var lv ={
         };
         f.getParams = function () { return paramNames; };
         return f;
+    },
+    insertionSort(ary){
+         //todo
     }
 };
